c1869b711a749a4be0eadde7de29b25a612682f7
James Helton <trealtrix@gmail.com>
2016-06-04 10:34:09 -0400

    Add tagging feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,14 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list
+    tags.join(', ')
+  end
+
+  def tag_list=(list_of_tags)
+    uniq_tags = list_of_tags.split(',').collect { |t| t.strip.downcase }
+    self.tags = uniq_tags.collect { |t| Tag.find_or_create_by(name: t) }
+  end
 end
